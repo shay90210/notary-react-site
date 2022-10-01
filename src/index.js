b9c6a5aa8caa2753c27f2a2bf4be65af7bbb2047
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-01 16:12:58 -0500

    removed code and added scss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import App from './App';
 
 import { createRoot } from 'react-dom/client';
 
+import './App.scss';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
