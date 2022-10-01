37b461bef388e4788f8e6822da2d326fb1c65ffe
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-01 16:10:42 -0500

    removed code

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import App from './App';
 
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-
-
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
