af4d6ca1affd07dfb3a0fdc24a3078d67229a2ca
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-02 12:04:53 -0500

    navigation bar

--- src/app/components/NavigationBar.js
+++ src/app/components/NavigationBar.js
@@ -5,53 +5,59 @@ const NavigationBar = () => {
     const handleSelect = (eventKey) => alert(`selected $(eventKey)`);
 
     return (
-        <Nav
-            onSelect={handleSelect}
-            activeKey= '1'
-            className='main-nav'
-        >
-            <Nav.Item
-                className='nav-item'
-            >
-                <Nav.Link
-                    eventKey='1'
-                    href='#/home'
-                    className='nav-item'
-                >
-                    HOME
-                </Nav.Link>
-            </Nav.Item>
-                <NavDropdown
-                    title='ABOUT'
-                    id='nav-dropdown'
-                    className='nav-item'
-                >
-                    <NavDropdown.Item eventKey= '2.1'>About Our Company</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item eventKey= '2.2'>How Notary Works</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item eventKey= '2.3'>Information</NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown
-                    title='RESOURCES'
-                    id='nav-dropdown'
-                    className='nav-item'
-                >
-                    <NavDropdown.Item eventKey= '3.1'>Blog</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item eventKey= '3.2'>Knowledge Center</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Item
-                    className='nav-item'
-                >
-                    <Nav.Link
-                        eventKey='4'
-                        href='#/contact'
+        <div className = 'container'>
+            <div className='row justify-content-end'>
+                <div className='col'>
+                    <Nav
+                        onSelect={handleSelect}
+                        activeKey= '1'
+                        className='main-nav'
                     >
-                        CONTACT US
-                    </Nav.Link>
-                </Nav.Item>
-        </Nav>    
+                        <Nav.Item
+                            className='nav-item'
+                        >
+                            <Nav.Link
+                                eventKey='1'
+                                href='#/home'
+                                className='nav-item'
+                            >
+                                HOME
+                            </Nav.Link>
+                        </Nav.Item>
+                            <NavDropdown
+                                title='ABOUT'
+                                id='nav-dropdown'
+                                className='nav-item'
+                            >
+                                <NavDropdown.Item eventKey= '2.1'>About Our Company</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item eventKey= '2.2'>How Notary Works</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item eventKey= '2.3'>Information</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown
+                                title='RESOURCES'
+                                id='nav-dropdown'
+                                className='nav-item'
+                            >
+                                <NavDropdown.Item eventKey= '3.1'>Blog</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item eventKey= '3.2'>Knowledge Center</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Item
+                                className='nav-item'
+                            >
+                                <Nav.Link
+                                    eventKey='4'
+                                    href='#/contact'
+                                >
+                                    CONTACT US
+                                </Nav.Link>
+                            </Nav.Item>
+                    </Nav>    
+                </div>
+            </div>
+        </div>
    )
 }
 
