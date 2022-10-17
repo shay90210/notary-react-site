f4b48f62f7792e5be74b98bec6318afcaf69c265
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-16 19:29:23 -0500

    added a div

--- src/app/components/NavigationBar.js
+++ src/app/components/NavigationBar.js
@@ -3,38 +3,40 @@ import {  Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 const NavigationBar = () => {
 
     return (
-        <Navbar>
-            <NavbarBrand></NavbarBrand>
-            <Nav 
-                center
-                navbar
-            >
-                <NavItem>
-                    <NavLink 
-                        href='/'
-                        className='nav-link'
-                    >HOME</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink 
-                        href='/'
-                        className='nav-link'
-                    >ABOUT OUR COMPANY</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink 
-                        href='/'
-                        className='nav-link'
-                    >RESOURCES</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink 
-                        href='/'
-                        className='nav-link'
-                    >CONTACT US</NavLink>
-                </NavItem>
-            </Nav>
-        </Navbar>
+        <div>
+            <Navbar>
+                <NavbarBrand></NavbarBrand>
+                <Nav 
+                    center
+                    navbar
+                >
+                    <NavItem>
+                        <NavLink 
+                            href='/'
+                            className='nav-link'
+                        >HOME</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink 
+                            href='/'
+                            className='nav-link'
+                        >ABOUT OUR COMPANY</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink 
+                            href='/'
+                            className='nav-link'
+                        >RESOURCES</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink 
+                            href='/'
+                            className='nav-link'
+                        >CONTACT US</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </div>
     );
 }
 
