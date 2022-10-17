9abf0a0e7b7d325b46756899d673dfcfc410325e
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-16 19:13:28 -0500

    deleted and redid nav bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import NavigationBar from '../src/app/components/NavigationBar';
 
-import Background from '../src/app/assets/imgs/maingreenbackground.jpg';
-
 import './App.scss';
 
 function App() {
   return (
-    <div 
-      className="App"
-      style={{ 
-        backgroundImage: `url(${Background})`,
-        backgroundSize: 'cover',
-        width: '100'
-      }}
-    >
+    <div className="App">
         <NavigationBar />
     </div>
   );
--- src/app/components/NavigationBar.js
+++ src/app/components/NavigationBar.js
@@ -1,64 +1,20 @@
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import {  Nav, NavItem, NavLink } from 'reactstrap';
 
 const NavigationBar = () => {
-    const handleSelect = (eventKey) => alert(`selected $(eventKey)`);
 
     return (
-        <div className = 'container'>
-            <div className='row justify-content-end'>
-                <div className='col'>
-                    <Nav
-                        onSelect={handleSelect}
-                        activeKey= '1'
-                        className='main-nav'
-                    >
-                        <Nav.Item
-                            className='nav-item'
-                        >
-                            <Nav.Link
-                                eventKey='1'
-                                href='#/home'
-                                className='nav-item'
-                            >
-                                HOME
-                            </Nav.Link>
-                        </Nav.Item>
-                            <NavDropdown
-                                title='ABOUT'
-                                id='nav-dropdown'
-                                className='nav-item'
-                            >
-                                <NavDropdown.Item eventKey= '2.1'>About Our Company</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item eventKey= '2.2'>How Notary Works</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item eventKey= '2.3'>Information</NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown
-                                title='RESOURCES'
-                                id='nav-dropdown'
-                                className='nav-item'
-                            >
-                                <NavDropdown.Item eventKey= '3.1'>Blog</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item eventKey= '3.2'>Knowledge Center</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Item
-                                className='nav-item'
-                            >
-                                <Nav.Link
-                                    eventKey='4'
-                                    href='#/contact'
-                                >
-                                    CONTACT US
-                                </Nav.Link>
-                            </Nav.Item>
-                    </Nav>    
-                </div>
-            </div>
-        </div>
-   )
+        <Nav>
+            <NavItem>
+                <NavLink href=''>ABOUT OUR COMPANY</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href=''>RESOURCES</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href=''>CONTACT US</NavLink>
+            </NavItem>
+        </Nav>
+    );
 }
 
 export default NavigationBar; 
