72a8f58ac32b30be687999a16f145d35cdc210d9
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-16 20:06:18 -0500

    added routes, images and homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import NavigationBar from '../src/app/components/NavigationBar';
 
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './app/pages/HomePage';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
         <NavigationBar />
+          <Routes>
+            <Route path='/' exact element={<HomePage />} />
+          </Routes>
     </div>
   );
 }
