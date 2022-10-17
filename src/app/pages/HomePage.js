5411cb329b204c44e9f5754d4bbfbd24979d4355
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-16 20:09:58 -0500

    added homepage

---
+++ src/app/pages/HomePage.js
@@ -0,0 +1,13 @@
+import HomeBackground from '../assets/imgs/mainbackground.jpg';
+
+const HomePage = () => {
+    return (
+        <div
+            style={{ backgroundImage: `url(${HomeBackground})` }}
+        >
+            
+        </div>
+    );
+}
+
+export default HomePage;
