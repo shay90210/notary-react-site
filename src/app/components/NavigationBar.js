fde27775a9968979f8ef4875727a4a5a7f0bd777
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-16 19:17:33 -0500

    added navbar and navbarbrand

--- src/app/components/NavigationBar.js
+++ src/app/components/NavigationBar.js
@@ -1,19 +1,28 @@
-import {  Nav, NavItem, NavLink } from 'reactstrap';
+import {  Navbar, Navbarbrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 const NavigationBar = () => {
 
     return (
-        <Nav>
-            <NavItem>
-                <NavLink href=''>ABOUT OUR COMPANY</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href=''>RESOURCES</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href=''>CONTACT US</NavLink>
-            </NavItem>
-        </Nav>
+        <Navbar>
+            <Navbarbrand></Navbarbrand>
+            <Nav 
+                center
+                navbar
+            >
+                <NavItem>
+                    <NavLink href='/'>HOME</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href='/'>ABOUT OUR COMPANY</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href='/'>RESOURCES</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href='/'>CONTACT US</NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
     );
 }
 
