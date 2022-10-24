bd9d86457e1d54bfc9f547591ef877bcad1f0743
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-23 21:47:35 -0500

    remodeled entire contact form

--- src/app/components/ContactForm.js
+++ src/app/components/ContactForm.js
@@ -1,22 +1,26 @@
-import { Button, Label, Col, FormGroup, Card, CardBody } from 'reactstrap';
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import { validateContactForm } from '../utils/validateContactForm';
-import { useRef } from 'react';
+import { 
+    Button, 
+    Label, 
+    Col, 
+    FormGroup, 
+    Form, 
+    Input,  
+} from 'reactstrap';
 
-import emailjs from '@emailjs/browser';
+import * as emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
-    const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_k0z3tdi', 'template_enbqqht', form.current, 'HsQ73zypgOlmQBhhb')
+        emailjs.sendForm('service_83toxj7', 'template_enbqqht', e.target, 'HsQ73zypgOlmQBhhb')
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
                 console.log(error.text);
             });
+            e.target.reset();
     }
 
     return (
@@ -40,130 +44,148 @@ const ContactForm = () => {
             >
                 Please fill out the form below so that we can schedule the next available appointment for you!
             </h3>
-            <Formik
-                initialValues={{
-                    firstName: '',
-                    lastName: '',
-                    email: '',
-                    phoneNum: '',
-                    inPerson: false,
-                    virtual: false,
-                    realEstate: false,
-                }}
-                onSubmit={sendEmail}
-                validate={validateContactForm}
-                className='contact-form'
-                ref={form}
-            >
-                <Card>
-                    <Form>
-                        <CardBody>
-                            <FormGroup row>
-                                <Label htmlFor='firstName' md='2'>
-                                    First Name
-                                </Label>
-                                <Col md='6'>
-                                    <Field
-                                        name='firstName'
-                                        placeholder='First Name'
-                                        className='form-control'
-                                    />
-                                    <ErrorMessage name='firstName'>
-                                        {(msg) => <p className='text-danger'>{msg}</p>}
-                                    </ErrorMessage>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor='lastName' md='2'>
-                                    Last Name
-                                </Label>
-                                <Col md='6'>
-                                    <Field
-                                        name='lastName'
-                                        placeholder='Last Name'
-                                        className='form-control'
-                                    />
-                                    <ErrorMessage name='lastName'>
-                                        {(msg) => <p className='text-danger'>{msg}</p>}
-                                    </ErrorMessage>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor='email' md='2'>
-                                    Email Address
-                                </Label>
-                                <Col md='6'>
-                                    <Field
-                                        name='email'
-                                        placeholder='Email Address'
-                                        className='form-control'
-                                    />
-                                    <ErrorMessage name='email'>
-                                        {(msg) => <p className='text-danger'>{msg}</p>}
-                                    </ErrorMessage>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label htmlFor='phoneNum' md='2'>
-                                    Phone Number
-                                </Label>
-                                <Col md='6'>
-                                    <Field
-                                        name='phoneNum'
-                                        placeholder='Phone Number'
-                                        className='form-control'
-                                    />
-                                    <ErrorMessage name='phoneNum'>
-                                        {(msg) => <p className='text-danger'>{msg}</p>}
-                                    </ErrorMessage>
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label md='2'>
-                                    Notary Type
-                                </Label>
-                                <div role='group' aria-labelledby='checkbox-group'>
-                                    <label md='4'>
-                                        <Field 
-                                            type='checkbox'
-                                            name='checked'
-                                            value='inPerson'
-                                        />
-                                        In-Person
-                                    </label>
-                                    <label md='4'>
-                                        <Field 
-                                            type='checkbox'
-                                            name='checked'
-                                            value='virtual' 
-                                        />
-                                        Virtual
-                                    </label>
-                                    <label>
-                                    <Field 
-                                        type='checkbox'
-                                        name='checked'
-                                        value='realEstate' 
-                                    />
-                                    Real Estate
-                                    </label>
-                                </div>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Col md={{ size: 10, offset: 2 }}>
-                                    <Button 
-                                        type='submit' 
-                                        className='form-button'
-                                        style={{ backgroundColor: 'purple' }}
-                                    >
-                                        SUBMIT
-                                    </Button>
-                                </Col>
-                            </FormGroup>
-                        </CardBody>
-                    </Form>
-                </Card>
-            </Formik>
+                <Form
+                    onSubmit={sendEmail}
+                >
+                <FormGroup row>
+                    <Label
+                    for='firstName'
+                    sm={2}
+                    >
+                    First Name
+                    </Label>
+                    <Col sm={10}>
+                    <Input
+                        id='firstName'
+                        name='firstName'
+                        placeholder='First Name'
+                        type='text'
+                    />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                    for='lastName'
+                    sm={2}
+                    >
+                    LastName
+                    </Label>
+                    <Col sm={10}>
+                    <Input
+                        id='lastName'
+                        name='lastName'
+                        placeholder='Last Name'
+                        type='text'
+                    />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                    for='email'
+                    sm={2}
+                    >
+                    Email Address
+                    </Label>
+                    <Col sm={10}>
+                    <Input
+                        id='email'
+                        name='email'
+                        placeholder='Email Address'
+                        type='email'
+                    />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                    for='phoneNum'
+                    sm={2}
+                    >
+                    Phone Number
+                    </Label>
+                    <Col sm={10}>
+                    <Input
+                        id='phoneNum'
+                        name='phoneNum'
+                        placeholder='Phone Number'
+                        type='phoneNum'
+                    />
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label
+                    for="checkbox"
+                    sm={2}
+                    >
+                    Appointment Type
+                    </Label>
+                    <Col
+                    sm={{
+                        size: 2
+                    }}
+                    >
+                    <FormGroup check>
+                        <Input
+                        id="checkbox1"
+                        type="checkbox"
+                        />
+                        {' '}
+                        <Label check>
+                        In-Person
+                        </Label>
+                    </FormGroup>
+                    </Col>
+                    <Col
+                    sm={{
+                        size: 2
+                    }}
+                    >
+                    <FormGroup check>
+                        <Input
+                        id="checkbox2"
+                        type="checkbox"
+                        />
+                        {' '}
+                        <Label check>
+                        In-Person
+                        </Label>
+                    </FormGroup>
+                    </Col>
+                </FormGroup>
+                   <Col
+                    sm={{
+                        size: 2
+                    }}
+                    >
+                    <FormGroup check>
+                        <Input
+                        id="checkbox3"
+                        type="checkbox"
+                        />
+                        {' '}
+                        <Label check>
+                        Real Estate
+                        </Label>
+                    </FormGroup>
+                    </Col>
+                <FormGroup
+                    check
+                    row
+                >
+                    <Col
+                    sm={{
+                        offset: 2,
+                        size: 10
+                    }}
+                    >
+                    <Button
+                        type='submit'
+                        style={{ backgroundColor: '#561A68' }}
+                    >
+                        Submit
+                    </Button>
+                    </Col>
+                </FormGroup>
+                </Form>
         </div>
     )
 }
