bf520399daaf9051523589ba15e466ba76bae694
Shannon Bentley <bentley.shannon@yahoo.com>
2022-10-23 19:42:54 -0500

    footer update

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTiktok } from "@fortawesome/free-brands-svg-icons";
 
 import { Container, Col } from "react-bootstrap";
 
@@ -9,8 +10,9 @@ const Footer = () => {
             <footer>
                 <Container>
                     <Col>
-                        <h3>Social Media</h3>
+                        <h3>Connect with Us on Social Media</h3>
                         <a className='social-icons' href='https://www.instagram.com/nova_notary1/'><FontAwesomeIcon icon={faInstagram} /></a>      
+                        <a className='social-icons' href='https://www.tiktok.com/@novanotarydfw'><FontAwesomeIcon icon={faTiktok} /></a>      
                     </Col>
                 </Container>
             </footer>
